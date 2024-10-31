83c81113289db06aee6340730a417647f11ed743
Tadija <ztadija98@gmail.com>
2024-10-31 19:03:35 +0100

    renaming scripts and cleaning upgraded package json

--- my-workspace/rename-files.ts
+++ my-workspace/rename-files.ts
@@ -4,54 +4,36 @@ const path = require('path');
 // Set the path to your dist folder
 const distPath = path.join(__dirname, 'dist', 'app', 'browser');
 
-// Function to rename files
-const renameJsFiles = () => {
-  // Rename main.js to app.bundle.js
-  fs.rename(
-    path.join(distPath, 'main-KD7NPSAN.js'),  // Original file name
-    path.join(distPath, 'main.js'),  // New file name
-    (err) => {
-      if (err) {
-        console.error('Error renaming main.js:', err);
-      } else {
-        console.log('main.js renamed to app.bundle.js successfully!');
-      }
+// Function to rename files based on a pattern
+const renameFiles = (pattern, newName) => {
+  fs.readdir(distPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
     }
-  );
-};
-  const renamePolyFiles = () => {
-    // Rename main.js to app.bundle.js
-    fs.rename(
-      path.join(distPath, 'polyfills-FFHMD2TL.js'),  // Original file name
-      path.join(distPath, 'polyfills.js'),  // New file name
-      (err) => {
-        if (err) {
-          console.error('Error renaming polyfills.js:', err);
-        } else {
-          console.log('polyfills.js renamed to app.bundle.js successfully!');
+
+    // Find files that match the pattern
+    const matchedFiles = files.filter(file => file.match(pattern));
+    
+    // Rename each matched file
+    matchedFiles.forEach(file => {
+      const newFilePath = path.join(distPath, newName);
+      fs.rename(
+        path.join(distPath, file),
+        newFilePath,
+        (err) => {
+          if (err) {
+            console.error(`Error renaming ${file}:`, err);
+          } else {
+            console.log(`${file} renamed to ${newName} successfully!`);
+          }
         }
-      }
-    );
+      );
+    });
+  });
 };
 
-const renamecss = () => {
-    // Rename main.js to app.bundle.js
-    fs.rename(
-      path.join(distPath, 'styles-X2EKNRCY.css'),  // Original file name
-      path.join(distPath, 'styles.css'),  // New file name
-      (err) => {
-        if (err) {
-          console.error('Error renaming styles.css:', err);
-        } else {
-          console.log('styles.css renamed to app.bundle.css successfully!');
-        }
-      }
-    );
-};
-  // Rename other files if needed
-  // Add more fs.rename calls as required
-
-// Execute the renaming function
-renameJsFiles();
-renamePolyFiles();
-renamecss();
+// Rename JavaScript and CSS files
+renameFiles(/main-[A-Z0-9]+\.js$/, 'main.js'); // Rename main.js
+renameFiles(/polyfills-[A-Z0-9]+\.js$/, 'polyfills.js'); // Rename polyfills.js
+renameFiles(/styles-[A-Z0-9]+\.css$/, 'styles.css'); // Rename styles.css
